88081113b2f1023553fc36499b7e7f44c43a17e6
Bogdan Plieshka <bogdanplieshka@gmail.com>
2016-09-10 18:05:29 +0200

    fix failure

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.querySelector('.action').addEventListener('click', function () {
-	// this.insertAdjacentHTML('afterend', '<div class="text">You hear \'click\'</div>');
+	this.insertAdjacentHTML('afterend', '<div class="text">You hear \'click\'</div>');
 });
